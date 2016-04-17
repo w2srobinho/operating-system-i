c4e5cfa582e196d70c32a05bbdb0225cfcf6e5eb
Willian de Souza <willianstosouza@gmail.com>
2016-04-17 13:11:47 -0300

    Improved documets for describe the functions requested bu Teacher

--- ics/ic1/main.cpp
+++ ics/ic1/main.cpp
@@ -14,15 +14,26 @@ int main ()
     /* Repeat 5 times */
     for(int i = 0; i < 5; ++i) {
         cout << "Parent process " << getpid() << ": Creating child" << endl;
-        /* Duplicate this process */
-        pid = fork();
-        /* If there was an error on duplication then */
+        /**
+         * Duplicate this process 
+         * 
+         * Create the child-process
+         */
+        pid = fork(); 
+        /**
+         * If there was an error on duplication then 
+         * 
+         * Check error on fork create
+         */
         if(pid < 0) {
             cout << "Error on fork()" << endl;
             return -1;
         }  
         /* If child-process is running then */
         if(pid == 0) {
+            /**
+             * Run child-process
+             */
             cout << "Child process " << getpid() << ": Running" << endl;
             count++;
             /* Sleep for 1 second */
@@ -41,13 +52,18 @@ int main ()
         while(errno != ECHILD) {            
             /**
              * The waitpid(pid_t pid, int *stat_loc, int options) when parameter pid is equal
-             *     to -1, status is requested for any child process. In this respect,
-             *     waitpid() is equivalent to wait(). 
+             *     to -1, status is requested for any child process and options is equals 0.
+             *     In this respect, waitpid() is equivalent to wait().
+             * 
+             * The wait() system call suspends execution of the calling process until one of 
+             *     its children terminates.
              */
             if (wait(&status) > 0) {
                 /**                 
                  * If is child process
-                 * Increase return child process to sum variable
+                 * Increase sum variable with return child process
+                 * 
+                 * WEXITSTATUS - returns the exit status of the child.
                  */
                 sum += WEXITSTATUS(status);
             }
